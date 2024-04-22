4a43d889b67edbba6d10118e556352b970bb3e42
Resheba <74815401+Resheba@users.noreply.github.com>
2024-04-22 19:50:29 +0300

    Message Query Handler added

---
+++ src/infrastructure/queries/message.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+from src.infrastructure.repository.base.base import BaseRepository
+from src.infrastructure.queries.base import BaseQueryHandler
+from src.domain.entities.message import Message
+from src.domain.queries.message import GetMessageQuery
+
+
+@dataclass(frozen=True, eq=False)
+class GetMessageQueryHandler(BaseQueryHandler[GetMessageQuery, Tuple[Message]]):
+    repository: BaseRepository
+
+    async def handle(self, query: GetMessageQuery) -> Tuple[Message]:
+        result = await self.repository.get(
+            id=query.id,
+            message=query.message,
+            user=query.user_id,
+        )
+        return tuple(Message(**message) for message in result)
---
+++ tests/infrastructure/queries/test_message_query_handler.py
@@ -0,0 +1,22 @@
+import pytest
+
+from src.infrastructure.queries.message import GetMessageQuery, GetMessageQueryHandler
+
+
+@pytest.fixture(scope='class')
+def query_handler(repo):
+    return GetMessageQueryHandler(repository=repo)
+
+
+class TestGetMessageQueryHandler:
+    @pytest.mark.asyncio
+    async def test_get_message_query_handler_success(self, query_handler: GetMessageQueryHandler):
+        assert query_handler
+        assert await query_handler.handle(GetMessageQuery()) == ()
+
+    @pytest.mark.asyncio
+    async def test_get_message_query_handler_invalid_query(self, query_handler: GetMessageQueryHandler):
+        with pytest.raises(TypeError):
+            await query_handler.handle()
+
+
