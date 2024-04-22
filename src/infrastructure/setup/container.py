7d9c83a40e1f0ea20487e211ec038e32d0c71316
Resheba <74815401+Resheba@users.noreply.github.com>
2024-04-22 23:28:14 +0300

    Init Container DI #1

--- src/infrastructure/setup/container.py
+++ src/infrastructure/setup/container.py
@@ -28,7 +28,38 @@ from src.infrastructure.queries import (
     GetMessageQuery,
     GetMessageQueryHandler,
 )
+from src.infrastructure.mediator import Mediator
+
+
+def setup_mediator() -> Mediator:
+    mediator: Mediator = Mediator()
+    container: Container = setup_container()
+
+    mediator.register_command(CreateMessageCommand, [container.resolve(CreateMessageCommandHandler)])
+    mediator.register_command(CreatePostCommand, [container.resolve(CreatePostCommandHandler)])
+    mediator.register_command(CreateUserCommand, [container.resolve(CreateUserCommandHandler)])
+    mediator.register_command(DeletePostCommand, [container.resolve(DeletePostCommandHandler)])
+    mediator.register_command(DeleteUserCommand, [container.resolve(DeleteUserCommandHandler)])
+    mediator.register_query(GetUserQuery, [container.resolve(GetUserQueryHandler)])
+    mediator.register_query(GetPostQuery, [container.resolve(GetPostQueryHandler)])
+    mediator.register_query(GetMessageQuery, [container.resolve(GetMessageQueryHandler)])
+
+    return mediator
 
 
 def setup_container() -> Container:
-    ...
+    container: Container = Container()
+    container.register(BaseMessageRepository, MessageMemoryRepository)
+    container.register(BasePostRepository, PostMemoryRepository)
+    container.register(BaseUserRepository, UserMemoryRepository)
+    
+    container.register(CreateMessageCommandHandler)
+    container.register(CreatePostCommandHandler)
+    container.register(CreateUserCommandHandler)
+    container.register(DeletePostCommandHandler)
+    container.register(DeleteUserCommandHandler)
+    container.register(GetUserQueryHandler)
+    container.register(GetPostQueryHandler)
+    container.register(GetMessageQueryHandler)
+    
+    return container
